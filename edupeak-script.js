69dec07cb4e261d0da2db2b358d34c1b299add17
Chowmin King <aroraarjoon79@gmail.com>
2026-03-27 17:44:56 +0530

    responsiveness2

--- edupeak-script.js
+++ edupeak-script.js
@@ -1,7 +1,7 @@
 function closePopup() {
     const popup = document.getElementById('popup');
     popup.style.opacity = '0';
-    popup.style.transition = 'opacity 0.3s';
+    popup.style.transition = 'opacity 0.3s ease';
     setTimeout(() => {
         popup.style.display = 'none';
     }, 300);
